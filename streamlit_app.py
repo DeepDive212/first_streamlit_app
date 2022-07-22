05c9291f3d3eff71e4259f6f92af97f68b7ad698
Deep DIve <109364141+DeepDive212@users.noreply.github.com>
2022-07-21 22:01:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,9 +64,10 @@ def insert_row_snowflake(new_fruit):
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return "Thanks for adding " + new_fruit
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+add_my_fruit = insert_row_snowflake()
 
 # streamlit.stop()
 
 #This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
